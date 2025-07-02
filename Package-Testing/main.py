50dcee8c39dd26b42ab2d44241139b877adfae2e
Lakhini Voshadee Abeysundara <lakhinivoshadee24@gmail.com>
2025-07-02 16:55:07 +0530

    Day - 05

---
+++ Package-Testing/main.py
@@ -0,0 +1,31 @@
+
+# 01) 
+#import my_calculator.addition   
+#import my_calculator.subtraction
+
+
+# 02)
+#from my_calculator import addition
+#from my_calculator import subtraction
+
+# 03) 
+#from my_calculator.addition import add
+#from my_calculator.substraction import sub
+
+#from my_calculator import add, sub
+
+
+import my_calculator
+
+print("Package my_calculator imported successfully")
+print(my_calculator.add(5, 3))
+print(my_calculator.sub(5, 3))
+
+
+
+#print("Addition:", add(5, 3))
+#print("Subtraction:", sub(5, 3))
+
+
+#print(add(5, 3))  
+#print(sub(5, 3))

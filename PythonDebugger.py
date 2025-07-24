18b59e1e1de5026dd34ffd0425942139bb671352
Lakhini Voshadee Abeysundara <lakhinivoshadee24@gmail.com>
2025-07-24 18:20:25 +0530

    Day-08

---
+++ PythonDebugger.py
@@ -0,0 +1,10 @@
+
+
+def multiplication(a, b):
+    return a * b
+
+breakpoint()  # Set a breakpoint for debugging
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+result = multiplication(x,y)
+print("The result is:", result)
